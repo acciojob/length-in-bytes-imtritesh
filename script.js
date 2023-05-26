0756185f71d9d9a97bc3d4c0bc44ff3972e8ae3b
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-26 12:41:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const byteSize = (str) => {
+function byteSize(str){
   // write your code here
+  return Buffer.byteLength(str, 'utf8'); 
 };
-
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
